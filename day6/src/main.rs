7e6c796a3ef64927eb0655a099b2db25aa0cb7d5
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-12-06 08:36:14 +0200

    Day6 part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 use std::fs::OpenOptions;
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Seek};
 use anyhow::{Result, anyhow};
 
 #[derive(Debug)]
@@ -34,11 +34,9 @@ fn distance(hold_time: usize, time: usize) -> usize {
     hold_time * (time - hold_time)
 }
 
-fn main() -> Result<()> {
-    let file = OpenOptions::new()
-        .read(true)
-        .open("input")?;
-    let mut lines = BufReader::new(file).lines();
+fn part_one<R: BufRead>(reader: &mut R) -> Result<()> {
+    let mut lines = reader.lines();
+
     let times: Vec<usize> = lines.next()
         .ok_or(anyhow!("Missing times in input"))??
         .strip_prefix("Time:")
@@ -46,6 +44,7 @@ fn main() -> Result<()> {
         .split_ascii_whitespace()
         .map(|value|value.parse::<usize>())
         .collect::<Result<_, _>>()?;
+
     let distances: Vec<usize> = lines.next()
         .ok_or(anyhow!("Missing distances in input"))??
         .strip_prefix("Distance:")
@@ -53,11 +52,52 @@ fn main() -> Result<()> {
         .split_ascii_whitespace()
         .map(|value|value.parse::<usize>())
         .collect::<Result<_, _>>()?;
+
     let races: Vec<Race> = times.into_iter()
         .zip(distances.into_iter())
         .map(|(time, record_distance)| Race {time, record_distance})
         .collect();
+
     let product: usize = races.iter().map(|race|race.record_beat_ways_count()).product();
+
     println!("{product}");
+    Ok(())
+}
+
+fn part_two<R: BufRead + Seek>(reader: &mut R) -> Result<()> {
+    reader.rewind();
+    let mut lines = reader.lines();
+
+    let time: usize = lines.next()
+        .ok_or(anyhow!("Missing times in input"))??
+        .strip_prefix("Time:")
+        .ok_or(anyhow!("Invalid input format"))?
+        .replace(' ', "")
+        .parse()?;
+
+    let record_distance: usize = lines.next()
+        .ok_or(anyhow!("Missing times in input"))??
+        .strip_prefix("Distance:")
+        .ok_or(anyhow!("Invalid input format"))?
+        .replace(' ', "")
+        .parse()?;
+
+    let race = Race { time, record_distance };
+    let count = race.record_beat_ways_count();
+
+    println!("{count}");
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .open("input")?;
+    let mut reader = BufReader::new(file);
+
+    part_one(&mut reader)?;
+    part_two(&mut reader)?;
+
     Ok(())
 }
